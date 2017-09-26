4c35f40a079f1dd62ee0b6763454868e265f7b34
Levente Belteky <belteky.levente@gmail.com>
2017-09-27 07:42:46 +0800

    feat(backend): serve static files from dist/

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,12 @@
 'use strict';
 
 const express = require('express');
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+app.use(express.static(path.resolve(__dirname, '../../dist')));
 
 app.get('/heartbeat', (req, res) => {
   res.json({status: 'ok'});
