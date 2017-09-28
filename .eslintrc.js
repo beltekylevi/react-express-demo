cec6cb00261bb4dd2a812dc9cb8daa2bd4e6280f
Levente Belteky <belteky.levente@gmail.com>
2017-09-28 23:18:05 +0800

    chore(linter): set the proper environments
     -  remove node and browser environment from the global config
     -  add no-undef rule
     -  specify which files needs the node/browser/mocha/jest env

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,6 @@ module.exports = {
   'extends': 'google',
   'env': {
     'es6': true,
-    'browser': true,
-    'node': true,
   },
   'parserOptions': {
     'ecmaFeatures': {
@@ -14,6 +12,7 @@ module.exports = {
     'require-jsdoc': 0,
     'react/jsx-uses-vars': 2,
     'react/jsx-uses-react': 2,
+    'no-undef': 2,
     'indent': [2, 2],
   },
   'plugins': [
@@ -24,6 +23,33 @@ module.exports = {
       'files': ['src/client/**/*.js'],
       'parserOptions': {
         'sourceType': 'module',
+      },
+      'env': {
+        'browser': true,
+      },
+    },
+    {
+      'files': [
+        'webpack.*.js',
+        '.eslintrc.js',
+        'jest.config.js',
+        'test/**/*.test.js',
+        'src/server/**/*.js',
+      ],
+      'env': {
+        'node': true,
+      },
+    },
+    {
+      'files': ['src/client/**/*.test.js'],
+      'env': {
+        'jest': true,
+      },
+    },
+    {
+      'files': ['test/**/*.test.js'],
+      'env': {
+        'mocha': true,
       },
     },
   ],
