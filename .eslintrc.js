a32f7829e032460420bdb14480cb94add7ed0f8e
Levente Belteky <belteky.levente@gmail.com>
2017-09-28 22:59:52 +0800

    chore(linter): set eslint parser to default
    babel-eslint itself suggests that you shouldn't use their parser
    just because you are using babel.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,30 @@
 module.exports = {
   'extends': 'google',
-  'parser': 'babel-eslint',
   'env': {
+    'es6': true,
     'browser': true,
     'node': true,
+  },
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true,
+    },
   },
   'rules': {
     'require-jsdoc': 0,
     'react/jsx-uses-vars': 2,
     'react/jsx-uses-react': 2,
+    'indent': [2, 2],
   },
   'plugins': [
     'react',
+  ],
+  'overrides': [
+    {
+      'files': ['src/client/**/*.js'],
+      'parserOptions': {
+        'sourceType': 'module',
+      },
+    },
   ],
 };
