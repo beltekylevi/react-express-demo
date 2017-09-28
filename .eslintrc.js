98e691fbcbdd20011918830296685f8ba9290392
Levente Belteky <belteky.levente@gmail.com>
2017-09-28 11:13:40 -0500

    Improve development environment (#2)
    Improve development environment

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,56 @@
 module.exports = {
   'extends': 'google',
-  'parser': 'babel-eslint',
   'env': {
-    'browser': true,
-    'node': true,
+    'es6': true,
+  },
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true,
+    },
   },
   'rules': {
     'require-jsdoc': 0,
     'react/jsx-uses-vars': 2,
     'react/jsx-uses-react': 2,
+    'no-undef': 2,
+    'indent': [2, 2],
   },
   'plugins': [
     'react',
+  ],
+  'overrides': [
+    {
+      'files': ['src/client/**/*.js'],
+      'parserOptions': {
+        'sourceType': 'module',
+      },
+      'env': {
+        'browser': true,
+      },
+    },
+    {
+      'files': [
+        'webpack.*.js',
+        '.eslintrc.js',
+        'jest.config.js',
+        'test/**/*.test.js',
+        'src/server/**/*.js',
+      ],
+      'env': {
+        'node': true,
+      },
+    },
+    {
+      'files': ['src/client/**/*.test.js'],
+      'env': {
+        'jest': true,
+      },
+    },
+    {
+      'files': ['test/**/*.test.js'],
+      'env': {
+        'mocha': true,
+      },
+    },
   ],
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,15 @@
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const webpack = require('webpack');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   plugins: [
     new UglifyJSPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production'),
+      },
+    }),
   ],
 });
